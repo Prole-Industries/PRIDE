11b326e795b48ab854b3e9aa7ae92ef9348e2443
Johann Scott <75587365+somecollagist@users.noreply.github.com>
2021-03-02 14:17:06 +0000

    fixed some keywords being recognised as methods

--- index.js
+++ index.js
@@ -1,133 +1,18 @@
-//These are the main processes
+const { Menu } = require('electron');
+const Main = require('./main');
 
-const {app, BrowserWindow, Menu, remote} = require('electron');
+//Connect the click-binds to the menu, because document doesn't work in the main renderer.
 
-//Keeps a global reference of the window so it doesn't get yoten by the binman
-let mainWindow;
-
-//Because Apple never do things easy, we should keep an eye out to check if we're running on OSX
-let isMac = process.platform === 'darwin';
-
-const createWindow = () => {
-    //Create browser window
-    mainWindow = new BrowserWindow({
-        width: 1080,
-        height: 720,
-        frame: false,
-        titleBarStyle: 'hidden',
-        webPreferences: {
-            enableRemoteModule: true,
-            nodeIntegration: true
-        }
+//Open bind
+Main.Menu[0].submenu[1].click = () => {
+    dialog.showOpenDialog({ properties: ['openFile']}).then((result) => {
+        let path = result.filePaths[0];
+        document.getElementById("filepath").innerText = path;
+        console.log(document.editor);
     });
-
-    //Prep the titlebar
-    Menu.setApplicationMenu(Menu.buildFromTemplate([
-        {
-          label: "File",
-          submenu: [
-            {
-              label: "New",
-              accelerator: "CmdOrCtrl+N"
-            },
-            {
-              label: "Open",
-              accelerator: "CmdOrCtrl+O"
-            },
-            {
-              label: "Save",
-              accelerator: "CmdOrCtrl+S"
-            },
-            {
-              label: "Save As",
-              accelerator: "CmdOrCtrl+Shift+S"
-            },
-            {type: "separator"},
-            {
-              label: "Fullscreen",
-              accelerator: "F11",
-              click: () => {
-                mainWindow.setFullScreen(!mainWindow.fullScreen);
-              }
-            },
-          ]
-        },
-        {
-          label: "Edit",
-          submenu: [
-            {
-              label: "Undo",
-              role: "undo",
-              accelerator: "CmdOrCtrl+Z"
-            },
-            {
-              label: "Redo",
-              role: "redo",
-              accelerator: "CmdOrCtrl+Shift+Z"
-            },
-            {type: "separator"},
-            {
-              label: "Cut",
-              role: "cut",
-              accelerator: "CmdOrCtrl+X"
-            },
-            {
-              label: "Copy",
-              role: "copy",
-              accelerator: "CmdOrCtrl+C"
-            },
-            {
-              label: "Paste",
-              role: "paste",
-              accelerator: "CmdOrCtrl+V"
-            },
-          ]
-        },
-        {
-          label: "Options",
-          submenu: [
-            {
-              label: "Options Menu",
-              accelerator: "CmdOrCtrl+Alt+M"
-            },
-            {type: "separator"},
-            {
-              label: "Help",
-              accelerator: "CmdOrCtrl+?"
-            },
-            {
-              label: "Docs",
-              accelerator: "CmdOrCtrl+Alt+D"
-            },
-            {
-              label: "Reload",
-              role: "reload",
-              accelerator: "CmdOrCtrl+R"
-            }
-          ]
-        },
-        {
-          label: "Compile",
-          accelerator: "F5"
-        }
-    ]));
-
-    //Open the main splash screen
-    mainWindow.loadFile('index.html');
 }
 
-//Called after initialisation, we're ready to make windows
-app.whenReady().then(() => {
-    createWindow();
-});
-
-//Quit when all windows are closed if it's OSX
-app.on('window-all-closed', () => {
-    //I told you Apple were difficult. Mac doesn't close programs unless the user pressed Cmd+Q
-    if(!isMac) app.quit();
-});
-
-app.on('activate', () => {
-    //ffs Apple, I don't even know
-    if(mainWindow === null) createWindow();
-});
+//Fullscreen bind
+Main.Menu[0].submenu[5].click = () => {
+    mainWindow.setFullScreen(!mainWindow.fullScreen);
+}
